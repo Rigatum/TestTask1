9c1f362b5bbe94219551b8cf01cfd0258733018c
Tagir Melnikov <Melnikov@gmail.ru>
2023-04-20 10:41:58 +0700

    add Flat model

---
+++ TestTask1/Models/Flat.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestTask1.Models
+{
+    public class Flat
+    {
+        public int ID { get; set; }
+        public string FlatName { get; set; }
+        public string FlatType { get; set; }
+        public string FlatTypeFull { get; set; }
+        public int HouseID { get; set; }
+        public House House { get; set; }
+        public ICollection<Owner> Owners { get; set;}
+
+        public override string ToString()
+        {
+            return $"{FlatType}. {FlatName}";
+        }
+    }
+}
