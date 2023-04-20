f4ab7bc562a40e9839736a7001fee5256fea8ebd
Tagir Melnikov <Melnikov@gmail.ru>
2023-04-20 10:22:36 +0700

    Create folder Models and add City class to it

---
+++ TestTask1/Models/City.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestTask1.Models
+{
+    public class City
+    {
+        public int ID { get; set; }
+        public string CityName { get; set; }
+        public string CityType { get; set; }
+        public string CityTypeFull { get; set; }
+
+        public override string ToString()
+        {
+            return $"{CityType}. {CityName}";
+        }
+    }
+}
