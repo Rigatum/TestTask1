b67c0f5eb9a5d990e94f76cbedcc1fbb9274e56d
Tagir Melnikov <Melnikov@gmail.ru>
2023-04-21 12:46:31 +0700

    inject AddressService to model ManageObjects

--- TestTask1/Pages/ManageObjects.cshtml.cs
+++ TestTask1/Pages/ManageObjects.cshtml.cs
@@ -1,19 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using TestTask1.Services;
+using Dadata.Model;
+using TestTask1.Contracts;
 namespace TestTask1.Pages;
 
 public class ManageObjectsModel : PageModel
 {
+    private IAddressService _addressService; 
     private readonly ILogger<ManageObjectsModel> _logger;
+    public Address address;
+    
 
-    public ManageObjectsModel(ILogger<ManageObjectsModel> logger)
+    public ManageObjectsModel(ILogger<ManageObjectsModel> logger, IAddressService addressService)
     {
         _logger = logger;
+        _addressService = addressService;
     }
 
     public void OnGet()
     {
+        address = _addressService.GetCleanAddress("");
     }
 }
 
