c419d940fd08ebf2a89de278c58cf01c8bdbfeba
Tagir Melnikov <Melnikov@gmail.ru>
2023-04-21 12:45:29 +0700

    Inject context db to index model

--- TestTask1/Pages/Index.cshtml.cs
+++ TestTask1/Pages/Index.cshtml.cs
@@ -1,19 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TestTask1.Data;
+using TestTask1.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace TestTask1.Pages;
 
 public class IndexModel : PageModel
 {
+    ApplicationContext context;
+    public List<City> Cities { get; private set; } = new();
     private readonly ILogger<IndexModel> _logger;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, ApplicationContext db)
     {
         _logger = logger;
+        context = db;
     }
 
     public void OnGet()
     {
-
+        Cities = context.Cities.Take(10).ToList();
     }
 }
