7270782ccbe7c9faafadd17a53d88a1c88dbd446
Tagir Melnikov <Melnikov@gmail.ru>
2023-04-22 10:01:46 +0700

    add namespaces and db instance

--- TestTask1/Pages/Addresses/Add.cshtml.cs
+++ TestTask1/Pages/Addresses/Add.cshtml.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using TestTask1.Contracts;
 using TestTask1.Models;
+using TestTask1.Pages;
+using TestTask1.Data;
 namespace TestTask1.Pages.Addresses
 {
     public class Add : PageModel
@@ -14,10 +16,12 @@ namespace TestTask1.Pages.Addresses
         [BindProperty]
         public string address { get; set; }
         private readonly ILogger<Add> _logger;
+        private readonly ApplicationContext _db;
 
-        public Add(ILogger<Add> logger)
+        public Add(ILogger<Add> logger, ApplicationContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public void OnGet()
