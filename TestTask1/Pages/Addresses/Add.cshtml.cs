a96fa61f3e84d5f5f257cb96dcf120b3a0842e01
Tagir Melnikov <Melnikov@gmail.ru>
2023-04-22 09:26:00 +0700

    remove call address service

--- TestTask1/Pages/Addresses/Add.cshtml.cs
+++ TestTask1/Pages/Addresses/Add.cshtml.cs
@@ -15,14 +15,12 @@ namespace TestTask1.Pages.Addresses
         public string address { get; set; }
         public string token { get; set; }
         private readonly ILogger<Add> _logger;
-        private readonly IAddressService _addressService;
         private readonly DadataClientSettings _dadataClientSettings;
         private readonly IConfiguration _configuration;
 
-        public Add(ILogger<Add> logger, IAddressService addressService, IConfiguration Configuration)
+        public Add(ILogger<Add> logger, IConfiguration Configuration)
         {
             _logger = logger;
-            _addressService = addressService;
             _configuration = Configuration;
             _dadataClientSettings = _configuration.GetSection("DadataClient").Get<DadataClientSettings>();
             token = _dadataClientSettings.token;
@@ -34,8 +32,7 @@ namespace TestTask1.Pages.Addresses
 
         public void OnPost()
         {
-            var test = _addressService.GetCleanAddress(address);
-            System.Console.WriteLine(test.city);
+            
         }
     }
 }
