9f962aad27e016ee88a9fd61339ebb636fdc3688
Tagir Melnikov <Melnikov@gmail.ru>
2023-04-23 05:49:05 +0700

    add new method in interface for AddressService

--- TestTask1/Contracts/IAddressService.cs
+++ TestTask1/Contracts/IAddressService.cs
@@ -2,12 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Dadata.Model;
 
 namespace TestTask1.Contracts
 {
     public interface IAddressService
     {
-        Address GetCleanAddress(string dirtyAdress);
+        public Task<string> Insert(string addCityViewModelCityName, string addStreetViewModelStreetName, string addHouseViewModelHouseName,
+                                string addFlatViewModelFlatName, string addOwnerViewModelFIO);
     }
 }
