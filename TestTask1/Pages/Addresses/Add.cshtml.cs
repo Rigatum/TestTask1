a811a0b8e37c2a615ab0e81c41dc45d35d45b3ce
Tagir Melnikov <Melnikov@gmail.ru>
2023-04-22 09:13:08 +0700

    add CleanClient

--- TestTask1/Pages/Addresses/Add.cshtml.cs
+++ TestTask1/Pages/Addresses/Add.cshtml.cs
@@ -6,40 +6,26 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using TestTask1.Contracts;
-
+using TestTask1.Models;
 namespace TestTask1.Pages.Addresses
 {
-public class GameDataModel
-{
-    public string Name { get; set; }
-    public string ID { get; set; }
-
-    public List<GameDataModel> GetData()
-    {
-        List<GameDataModel> AutoCompleteData = new List<GameDataModel>()
-        {
-                new GameDataModel() { ID= "Game1", Name= "Badminton" },
-                new GameDataModel() { ID= "Game2", Name= "Basketball" },
-                new GameDataModel() { ID= "Game3", Name= "Cricket" },
-                new GameDataModel() { ID= "Game4", Name= "Football" },
-                new GameDataModel() { ID= "Game5", Name= "Golf" },
-                new GameDataModel() { ID= "Game6", Name= "Hockey" },
-                new GameDataModel() { ID= "Game7", Name= "Tennis"}
-        };
-        return AutoCompleteData;
-    }
-}
     public class Add : PageModel
     {
         [BindProperty]
-        public string dirtyAddress { get; set; }
+        public string address { get; set; }
+        public string token { get; set; }
         private readonly ILogger<Add> _logger;
         private readonly IAddressService _addressService;
+        private readonly DadataClientSettings _dadataClientSettings;
+        private readonly IConfiguration _configuration;
 
-        public Add(ILogger<Add> logger, IAddressService addressService)
+        public Add(ILogger<Add> logger, IAddressService addressService, IConfiguration Configuration)
         {
             _logger = logger;
             _addressService = addressService;
+            _configuration = Configuration;
+            _dadataClientSettings = _configuration.GetSection("DadataClient").Get<DadataClientSettings>();
+            token = _dadataClientSettings.token;
         }
 
         public void OnGet()
@@ -48,7 +34,7 @@ public class GameDataModel
 
         public void OnPost()
         {
-            var test = _addressService.GetCleanAddress(dirtyAddress);
+            var test = _addressService.GetCleanAddress(address);
             System.Console.WriteLine(test.city);
         }
     }
