e3dbee9e2290ba3beb19baefdcb6667d161e6eac
Tagir Melnikov <Melnikov@gmail.ru>
2023-04-22 15:53:35 +0700

    add using namespace

--- TestTask1/Data/ApplicationContext.cs
+++ TestTask1/Data/ApplicationContext.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using TestTask1.Models;
+using TestTask1.Models.Domain;
 
 namespace TestTask1.Data
 {
