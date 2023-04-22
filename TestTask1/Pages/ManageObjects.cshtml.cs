3ed01a87d57e836e1e77402b0d84ba52ee4fc957
Tagir Melnikov <Melnikov@gmail.ru>
2023-04-22 09:16:04 +0700

    delete address service

--- TestTask1/Pages/ManageObjects.cshtml.cs
+++ TestTask1/Pages/ManageObjects.cshtml.cs
@@ -7,20 +7,16 @@ namespace TestTask1.Pages;
 
 public class ManageObjectsModel : PageModel
 {
-    private IAddressService _addressService; 
     private readonly ILogger<ManageObjectsModel> _logger;
-    public Address address;
     
 
-    public ManageObjectsModel(ILogger<ManageObjectsModel> logger, IAddressService addressService)
+    public ManageObjectsModel(ILogger<ManageObjectsModel> logger)
     {
         _logger = logger;
-        _addressService = addressService;
     }
-
     public void OnGet()
     {
-        address = _addressService.GetCleanAddress("");
+        
     }
 }
 
