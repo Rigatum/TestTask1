b4861f64614353d53b95880c2dfa9c56c13e5109
Tagir Melnikov <tagirs1000@gmail.com>
2023-04-23 17:53:30 +0700

    Inject AddressService and call its method GetAddresses

--- TestTask1/Pages/ManageObjects.cshtml.cs
+++ TestTask1/Pages/ManageObjects.cshtml.cs
@@ -1,22 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using TestTask1.Services;
-using Dadata.Model;
+using Microsoft.Extensions.Logging;
+using TestTask1.Data;
+using TestTask1.Models.Domain;
 using TestTask1.Contracts;
 namespace TestTask1.Pages;
 
 public class ManageObjectsModel : PageModel
 {
     private readonly ILogger<ManageObjectsModel> _logger;
-    
-
-    public ManageObjectsModel(ILogger<ManageObjectsModel> logger)
+    private readonly IAddressService _addressService;
+    public List<Address> Addresses { get; set; }
+    public ManageObjectsModel(ILogger<ManageObjectsModel> logger, IAddressService addressService)
     {
         _logger = logger;
+        _addressService = addressService;
     }
     public void OnGet()
     {
-        
+        Addresses = _addressService.GetAddresses();
     }
 }
 
