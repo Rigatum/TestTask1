dae3e796d2a15035f01fde2d2c2c194c6e79e567
Tagir Melnikov <tagirs1000@gmail.com>
2023-04-23 17:56:38 +0700

    add new method IAddressService

--- TestTask1/Contracts/IAddressService.cs
+++ TestTask1/Contracts/IAddressService.cs
@@ -2,12 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using TestTask1.Models.Domain;
 namespace TestTask1.Contracts
 {
     public interface IAddressService
     {
         public Task<string> Insert(string addCityViewModelCityName, string addStreetViewModelStreetName, string addHouseViewModelHouseName,
                                 string addFlatViewModelFlatName, string addOwnerViewModelFIO);
+        public List<Address> GetAddresses();
     }
 }
