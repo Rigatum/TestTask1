3e67e7170cfdf1d05532bc559123d5c2fed6e02a
Tagir Melnikov <tagirs1000@gmail.com>
2023-04-23 17:55:00 +0700

    Inject AddressService and call its method getAddresses

--- TestTask1/Pages/Index.cshtml.cs
+++ TestTask1/Pages/Index.cshtml.cs
@@ -3,26 +3,22 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using TestTask1.Data;
 using TestTask1.Models.Domain;
 using Microsoft.EntityFrameworkCore;
-
+using TestTask1.Contracts;
 namespace TestTask1.Pages;
 
 public class IndexModel : PageModel
 {
-    private ApplicationContext _db;
-    public List<Street> Streets { get; private set; } = new();
     private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger, ApplicationContext db)
+    private readonly IAddressService _addressService;
+    public List<Address> Addresses { get; set; }
+    public IndexModel(ILogger<IndexModel> logger, IAddressService addressService)
     {
         _logger = logger;
-        _db = db;
+        _addressService = addressService;
     }
 
     public void OnGet()
     {
-        Streets = _db.Streets.Include(u => u.City).ToList();
-
-        foreach (var street in Streets)
-            Console.WriteLine($"{street.City.CityName} - {street.StreetName}");
+        Addresses = _addressService.GetAddresses();
     }
 }
