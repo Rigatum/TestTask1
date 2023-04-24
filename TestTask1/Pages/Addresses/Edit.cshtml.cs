ca09409a44646cc54c0f677c6cf5deac8f7b8c49
Tagir Melnikov <tagirs1000@gmail.com>
2023-04-24 13:30:08 +0700

    add call AddressService in edit page model

--- TestTask1/Pages/Addresses/Edit.cshtml.cs
+++ TestTask1/Pages/Addresses/Edit.cshtml.cs
@@ -5,20 +5,52 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-
+using TestTask1.Contracts;
+using TestTask1.Models.ViewModels;
 namespace TestTask1.Pages.Addresses
 {
     public class Edit : PageModel
     {
+        [BindProperty]
+        public EditCityViewModel editCityViewModel { get; set; }
+        [BindProperty]
+        public EditStreetViewModel editStreetViewModel { get; set; }
+        [BindProperty]
+        public EditHouseViewModel editHouseViewModel { get; set; }
+        [BindProperty]
+        public EditFlatViewModel editFlatViewModel { get; set; }
+        [BindProperty]
+        public EditOwnerViewModel editOwnerViewModel { get; set; }
         private readonly ILogger<Edit> _logger;
+        private readonly IAddressService _addressSerivce;
 
-        public Edit(ILogger<Edit> logger)
+        public Edit(ILogger<Edit> logger, IAddressService addressService)
         {
             _logger = logger;
+            _addressSerivce = addressService;
         }
 
         public void OnGet(int ID, int CityID, int StreetID, int HouseID, int FlatID)
         {
+            List<IViewModel> list;
+            _addressSerivce.ConvertToViewModel(ID, CityID, StreetID, HouseID, FlatID, out list);
+            
+            editCityViewModel = (EditCityViewModel)list[0];
+            editStreetViewModel = (EditStreetViewModel)list[1];
+            editHouseViewModel = (EditHouseViewModel)list[2];
+            editFlatViewModel = (EditFlatViewModel)list[3];
+            editOwnerViewModel = (EditOwnerViewModel)list[4];
+        }
+        public async Task<IActionResult> OnPost(int editCityViewModelID, int editStreetViewModelID, int editHouseViewModelID,
+                                 int editFlatViewModelID, int editOwnerViewModelID)
+        {
+            editCityViewModel.ID = editCityViewModelID;
+            editStreetViewModel.ID = editStreetViewModelID;
+            editHouseViewModel.ID = editHouseViewModelID;
+            editFlatViewModel.ID = editFlatViewModelID;
+            editOwnerViewModel.ID = editOwnerViewModelID;
+            ViewData["Message"] = await _addressSerivce.Update(editCityViewModel, editStreetViewModel, editHouseViewModel, editFlatViewModel, editOwnerViewModel);
+            return Page();
         }
     }
 }
