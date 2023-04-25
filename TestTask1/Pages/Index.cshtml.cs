2f89d10a5064a07a44f55c2d4396128867d6187e
Tagir Melnikov <tagirs1000@gmail.com>
2023-04-25 18:27:02 +0700

    Add calling GetCities method of AddressService

--- TestTask1/Pages/Index.cshtml.cs
+++ TestTask1/Pages/Index.cshtml.cs
@@ -4,6 +4,8 @@ using TestTask1.Data;
 using TestTask1.Models.Domain;
 using Microsoft.EntityFrameworkCore;
 using TestTask1.Contracts;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 namespace TestTask1.Pages;
 
 public class IndexModel : PageModel
@@ -19,8 +21,13 @@ public class IndexModel : PageModel
     public string CitySort { get; set; }
     public string HouseSort { get; set; }
     public string FlatNumberSort { get; set; }
-    public void OnGet(string sortOrder)
+    public string CurrentFilter { get; set; }
+    public List<SelectListItem> Cities { get; set; }
+    public void OnGet(string sortOrder, string searchString)
     {
+        CurrentFilter = searchString;
+        Cities = _addressService.GetCities();
+
         CitySort = sortOrder=="city_desc" ? "city_asc" : "city_desc";
         HouseSort = sortOrder=="house_desc" ? "house_asc" : "house_desc";
         FlatNumberSort = sortOrder=="flat_number_desc" ? "flat_number_asc" : "flat_number_desc";
