9c55aff416bad5eef785e7019ad5594440e7047d
Tagir Melnikov <tagirs1000@gmail.com>
2023-04-26 05:44:59 +0700

    add sort for street and new OnGetByHouse handler

--- TestTask1/Pages/Index.cshtml.cs
+++ TestTask1/Pages/Index.cshtml.cs
@@ -19,6 +19,7 @@ public class IndexModel : PageModel
         _addressService = addressService;
     }
     public string CitySort { get; set; }
+    public string StreetSort { get; set; }
     public string HouseSort { get; set; }
     public string FlatNumberSort { get; set; }
     public List<SelectListItem> Cities { get; set; }
@@ -29,9 +30,16 @@ public class IndexModel : PageModel
             Addresses = _addressService.FindAddressByCity(searchString);
         else {
             CitySort = sortOrder=="city_desc" ? "city_asc" : "city_desc";
+            StreetSort = sortOrder=="street_desc" ? "street_asc" : "street_desc";
             HouseSort = sortOrder=="house_desc" ? "house_asc" : "house_desc";
             FlatNumberSort = sortOrder=="flat_number_desc" ? "flat_number_asc" : "flat_number_desc";
             Addresses = _addressService.GetAddresses(sortOrder);
         }
+    }
+    public void OnGetByHouse()
+    {
+        Cities = _addressService.GetCities();
+        string s = (string)RouteData.Values["id"];
+        Addresses = _addressService.FindAddressByHouse(int.Parse(s));
     }
 }
