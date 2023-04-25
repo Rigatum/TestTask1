262ffa41faf3a4db90a62342acaf12af44eaddea
Tagir Melnikov <tagirs1000@gmail.com>
2023-04-25 19:19:10 +0700

    add using method FindAddressByCity

--- TestTask1/Pages/Index.cshtml.cs
+++ TestTask1/Pages/Index.cshtml.cs
@@ -21,16 +21,17 @@ public class IndexModel : PageModel
     public string CitySort { get; set; }
     public string HouseSort { get; set; }
     public string FlatNumberSort { get; set; }
-    public string CurrentFilter { get; set; }
     public List<SelectListItem> Cities { get; set; }
     public void OnGet(string sortOrder, string searchString)
     {
-        CurrentFilter = searchString;
         Cities = _addressService.GetCities();
-
-        CitySort = sortOrder=="city_desc" ? "city_asc" : "city_desc";
-        HouseSort = sortOrder=="house_desc" ? "house_asc" : "house_desc";
-        FlatNumberSort = sortOrder=="flat_number_desc" ? "flat_number_asc" : "flat_number_desc";
-        Addresses = _addressService.GetAddresses(sortOrder);
+        if (searchString != null)
+            Addresses = _addressService.FindAddressByCity(searchString);
+        else {
+            CitySort = sortOrder=="city_desc" ? "city_asc" : "city_desc";
+            HouseSort = sortOrder=="house_desc" ? "house_asc" : "house_desc";
+            FlatNumberSort = sortOrder=="flat_number_desc" ? "flat_number_asc" : "flat_number_desc";
+            Addresses = _addressService.GetAddresses(sortOrder);
+        }
     }
 }
