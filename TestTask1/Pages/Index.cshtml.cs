ff6f7fa174053d37305bdfc6ef622e8a7155ed74
Tagir Melnikov <tagirs1000@gmail.com>
2023-04-25 15:35:50 +0700

    get route value and pass it to GetAddresses service

--- TestTask1/Pages/Index.cshtml.cs
+++ TestTask1/Pages/Index.cshtml.cs
@@ -16,9 +16,21 @@ public class IndexModel : PageModel
         _logger = logger;
         _addressService = addressService;
     }
-
-    public void OnGet()
+    public string CitySort { get; set; }
+    public string StreetSort { get; set; }
+    public string HouseSort { get; set; }
+    public string FlatNumberSort { get; set; }
+    public string FlatSort { get; set; }
+    public string OwnerSort { get; set; }
+    public string CurrentSort { get; set; }
+    public void OnGet(string sortOrder)
     {
-        Addresses = _addressService.GetAddresses();
+        CitySort = sortOrder=="city_desc" ? "city_asc" : "city_desc";
+        StreetSort = sortOrder=="street_desc" ? "street_asc" : "street_desc";
+        HouseSort = sortOrder=="house_desc" ? "house_asc" : "house_desc";
+        FlatNumberSort = sortOrder=="flat_number_desc" ? "flat_number_asc" : "flat_number_desc";
+        FlatSort = sortOrder=="flat_desc" ? "flat_asc" : "flat_desc";
+        OwnerSort = sortOrder=="owner_desc" ? "owner_asc" : "owner_desc";
+        Addresses = _addressService.GetAddresses(sortOrder);
     }
 }
