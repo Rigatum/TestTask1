ded46b74de05c1cd691785660e0816e487585263
Tagir Melnikov <tagirs1000@gmail.com>
2023-04-25 15:49:24 +0700

    Remove Street, Flat, Owner from table

--- TestTask1/Pages/Index.cshtml.cs
+++ TestTask1/Pages/Index.cshtml.cs
@@ -17,20 +17,13 @@ public class IndexModel : PageModel
         _addressService = addressService;
     }
     public string CitySort { get; set; }
-    public string StreetSort { get; set; }
     public string HouseSort { get; set; }
     public string FlatNumberSort { get; set; }
-    public string FlatSort { get; set; }
-    public string OwnerSort { get; set; }
-    public string CurrentSort { get; set; }
     public void OnGet(string sortOrder)
     {
         CitySort = sortOrder=="city_desc" ? "city_asc" : "city_desc";
-        StreetSort = sortOrder=="street_desc" ? "street_asc" : "street_desc";
         HouseSort = sortOrder=="house_desc" ? "house_asc" : "house_desc";
         FlatNumberSort = sortOrder=="flat_number_desc" ? "flat_number_asc" : "flat_number_desc";
-        FlatSort = sortOrder=="flat_desc" ? "flat_asc" : "flat_desc";
-        OwnerSort = sortOrder=="owner_desc" ? "owner_asc" : "owner_desc";
         Addresses = _addressService.GetAddresses(sortOrder);
     }
 }
