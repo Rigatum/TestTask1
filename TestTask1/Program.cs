a70ff255661b2a7fc2d76a7fbd6068e58ea19869
Tagir Melnikov <tagirs1000@gmail.com>
2023-04-26 17:19:27 +0700

    Remove migration at startup and remove UseDeveloperExceptionPage form middleware

--- TestTask1/Program.cs
+++ TestTask1/Program.cs
@@ -21,27 +21,11 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-
-    var context = services.GetRequiredService<ApplicationContext>();
-    if (context.Database.GetPendingMigrations().Any())
-    {
-        context.Database.Migrate();
-    }
-}
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-else
-{
-    app.UseDeveloperExceptionPage();
+    app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
 
