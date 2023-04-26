ddc394c4040e92c54654b707f45f27a1823e03d3
Tagir Melnikov <tagirs1000@gmail.com>
2023-04-26 10:22:08 +0700

    add new sql script file which insert initial data to db

---
+++ sql/fill_tables.sql
@@ -0,0 +1,10 @@
+INSERT INTO City VALUES
+    (1, 'г Черногорск');
+INSERT INTO Street VALUES
+    (1, 'ул Совесткая', 1);
+INSERT INTO House VALUES
+    (1, 'д 42', 5, 1);
+INSERT INTO Flat VALUES
+    (1, 'кв 4', 1);
+INSERT INTO Owner VALUES
+    (1, 'Иванов Иван Иванович', 1);
